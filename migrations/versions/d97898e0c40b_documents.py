b8a82040b998c62f42a6fa1b32a47375c0e3ff54
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-08-10 20:36:11 +0200

    Format code

--- migrations/versions/d97898e0c40b_documents.py
+++ migrations/versions/d97898e0c40b_documents.py
@@ -9,21 +9,22 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision = 'd97898e0c40b'
-down_revision = '725802c4ebae'
+revision = "d97898e0c40b"
+down_revision = "725802c4ebae"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
     op.create_table(
-        'documents',
-        sa.Column('id', postgresql.UUID(), nullable=False, server_default=sa.text("gen_random_uuid()")),
-        sa.Column('encryption_key', sa.Text(), nullable=True),
-        sa.Column('name', sa.String(length=100), nullable=False),
-        sa.Column('path', sa.String(length=4), nullable=False),
-        sa.Column('created_at', postgresql.TIMESTAMP(timezone=True), nullable=True),
-        sa.PrimaryKeyConstraint('id'),
+        "documents",
+        sa.Column("id", postgresql.UUID(), nullable=False, server_default=sa.text("gen_random_uuid()")),
+        sa.Column("encryption_key", sa.Text(), nullable=True),
+        sa.Column("name", sa.String(length=100), nullable=False),
+        sa.Column("path", sa.String(length=4), nullable=False),
+        sa.Column("person_id", postgresql.UUID(), nullable=False),
+        sa.Column("created_at", postgresql.TIMESTAMP(timezone=True), nullable=True),
+        sa.PrimaryKeyConstraint("id"),
         sa.ForeignKeyConstraint(
             ("person_id",),
             ["displaced_persons.id"],
@@ -34,4 +35,4 @@ def upgrade():
 
 
 def downgrade():
-    op.drop_table('documents')
+    op.drop_table("documents")
