4157045a55af0df95d75878089d2aec347a4bdf8
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-09-01 21:20:27 +0200

    Add pets table

---
+++ observer/db/tables/pets.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, ForeignKey, Index, Table, Text, text
+from sqlalchemy.dialects.postgresql import TIMESTAMP, UUID
+
+from observer.db import metadata
+from observer.db.util import utcnow
+
+pets = Table(
+    "pets",
+    metadata,
+    Column("id", UUID, primary_key=True),
+    Column("name", Text(), nullable=False),
+    Column("notes", Text(), nullable=True),
+    Column("status", Text(), nullable=False),
+    Column("registration_id", Text(), nullable=True),
+    Column("created_at", TIMESTAMP(timezone=True), default=utcnow),
+    Column("updated_at", TIMESTAMP(timezone=True), default=utcnow, onupdate=utcnow),
+    Column("owner_id", UUID, ForeignKey("displaced_persons.id"), nullable=True),
+    Index("ix_animals_name", text("lower(name)")),
+)
