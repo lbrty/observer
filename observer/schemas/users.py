ffe1d669106995eb6d539dc7bc84dae6870ba7a8
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-10-15 17:29:24 +0200

    Add users list response schema

--- observer/schemas/users.py
+++ observer/schemas/users.py
@@ -12,8 +12,6 @@ class BaseUser(SchemaBase):
 
 class NewUser(BaseUser):
     password: SecretStr = Field(..., description="Password which user has provided")
-    is_active: bool = Field(True, description="Is user active?")
-    is_confirmed: bool = Field(False, description="Is user confirmed?")
 
 
 class UpdateUser(BaseUser):
@@ -34,9 +32,13 @@ class UserMFAUpdate(SchemaBase):
 
 class User(BaseUser):
     id: Identifier = Field(..., description="ID of user")
-    password_hash: SecretStr = Field(..., description="Hashed value of a password")
     is_active: bool = Field(True, description="Is user active?")
     is_confirmed: bool = Field(False, description="Is user confirmed?")
     mfa_enabled: bool = Field(False, description="Is MFA enabled for user?")
     mfa_encrypted_secret: SecretStr | None = Field(None, description="Secret value for MFA")
     mfa_encrypted_backup_codes: SecretStr | None = Field(None, description="Backup codes for MFA")
+
+
+class UsersResponse(SchemaBase):
+    total: int = Field(..., description="Total count of users")
+    items: list[User] = Field(..., description="List of users")
