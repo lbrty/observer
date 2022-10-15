83293f23cc5616cf805b88203b8314ec7ed6a569
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-10-15 21:38:04 +0200

    Add documents schemas

---
+++ observer/schemas/documents.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from pydantic import BaseModel, Field
+
+from observer.common.types import Identifier
+from observer.schemas.base import SchemaBase
+
+
+class BaseDocument(SchemaBase):
+    encryption_key: str | None = Field(None, description="Encryption key")
+    name: str = Field(..., description="Document filename")
+    path: str = Field(..., description="Document path")
+    owner_id: Identifier = Field(..., description="Owner ID")
+    created_at: datetime = Field(..., description="Creation date and time")
+
+
+class Document(BaseDocument):
+    id: Identifier = Field(..., description="Document ID")
+
+
+class DocumentsResponse(BaseModel):
+    total: int = Field(..., description="Total count of documents")
+    items: list[Document] = Field(..., description="List of documents")
