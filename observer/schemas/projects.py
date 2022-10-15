41b0ab90f3637d5be2f37d02585a57febecf02e0
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-10-15 17:39:01 +0200

    Add project filters

--- observer/schemas/projects.py
+++ observer/schemas/projects.py
@@ -4,6 +4,12 @@ from observer.common.types import Identifier
 from observer.schemas.base import SchemaBase
 
 
+class ProjectFilters(SchemaBase):
+    id: Identifier | None = Field(None, description="ID of project")
+    name: str | None = Field(None, description="Name of project")
+    description: str | None = Field(None, description="Description of project")
+
+
 class BaseProject(SchemaBase):
     name: str = Field(..., description="Name of project")
     description: str | None = Field(None, description="Description of project")
