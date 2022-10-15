5088425a908a408a369ace3d276dd24dc71003f0
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-10-15 18:05:01 +0200

    Adjust support records table

--- observer/db/tables/support_records.py
+++ observer/db/tables/support_records.py
@@ -14,7 +14,7 @@ projects = Table(
     Column("beneficiary_age", Text(), nullable=True),
     Column("owner_id", UUID(), nullable=False),
     Column("created_at", TIMESTAMP(timezone=True), default=utcnow, nullable=True),
-    CheckConstraint("type IN ('humanitarian', 'legal', 'general')", name="support_records_types"),
+    CheckConstraint("type IN ('humanitarian', 'legal', 'medical', 'general')", name="support_records_types"),
     CheckConstraint(
         "beneficiary_age IN ('0-1', '1-3', '4-5', '6-11', '12-14', '15-17', '18-25', '26-34', '35-59', '60-100+')",
         name="support_records_beneficiary_ages",
