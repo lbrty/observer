98e689164c8f4e59eb77aafff25d0de657970556
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-11-07 22:25:48 +0100

    Extract db proxy

--- observer/db/__init__.py
+++ observer/db/__init__.py
@@ -1,62 +1,14 @@
-from contextlib import asynccontextmanager
 from dataclasses import dataclass
-from typing import Any, Protocol
+from typing import Any
 
 from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import AsyncAdaptedQueuePool
 
+from observer.db.proxy import Database
+
 metadata = MetaData()
-
-
-class DbProxyInterface(Protocol):
-    async def fetchone(self, statement):
-        raise NotImplementedError
-
-    async def fetchall(self, statement):
-        raise NotImplementedError
-
-    async def execute(self, statement):
-        raise NotImplementedError
-
-    async def transaction(self):
-        raise NotImplementedError
-
-
-@dataclass
-class Database(DbProxyInterface):
-    engine: AsyncEngine
-    session: sessionmaker
-
-    async def fetchone(self, statement):
-        result = await self.execute(statement)
-        return result.fetchone()
-
-    async def fetchall(self, statement):
-        result = await self.execute(statement)
-        return result.fetchall()
-
-    async def execute(self, statement):
-        async with self.session.begin() as conn:
-            try:
-                result = await conn.execute(statement)
-                await conn.commit()
-                return result
-            except Exception:
-                await conn.rollback()
-                raise
-
-    @asynccontextmanager
-    async def transaction(self):
-        try:
-            async with self.session.begin() as conn:
-                yield
-                await conn.commit()
-        finally:
-            # making sure we call __aexit__ in case an exception is raised
-            #  by the code running under this contextmanager
-            pass
 
 
 @dataclass
