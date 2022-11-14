6daaaa89d5a620bdd1c428724300632e5cdac6dc
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-11-14 14:26:28 +0100

    Use database proxy

--- observer/repositories/users.py
+++ observer/repositories/users.py
@@ -21,20 +21,15 @@ class UsersRepository(UsersRepositoryInterface):
         self.db = db
 
     async def get_by_id(self, user_id: Identifier) -> User | None:
-        with self.db.session as conn:
-            query = select(users).where(users.c.id == user_id)
-
-            if result := await conn.execute(query):
-                row = result.fetchone()
-                return User(**row)
+        query = select(users).where(users.c.id == user_id)
+        if result := await self.db.fetchone(query):
+            return User(**result)
 
         return None
 
     async def get_by_ref_id(self, ref_id: Identifier) -> User | None:
-        with self.db.session as conn:
-            query = select(users).where(users.c.ref_id == ref_id)
-            if result := await conn.execute(query):
-                row = result.fetchone()
-                return User(**row)
+        query = select(users).where(users.c.ref_id == ref_id)
+        if result := await self.db.fetchone(query):
+            return User(**result)
 
         return None
