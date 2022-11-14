5675951ab0785ddd1ec69f2cd124ec2829e97c01
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-11-14 21:25:12 +0100

    Use pytest asyncio

--- tests/e2e_tests/conftest.py
+++ tests/e2e_tests/conftest.py
@@ -1,6 +1,17 @@
+import asyncio
+
 import pytest
-from fastapi import FastAPI
+import pytest_asyncio
 from dotenv import load_dotenv
+from fastapi import FastAPI
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    policy = asyncio.get_event_loop_policy()
+    loop = policy.new_event_loop()
+    yield loop
+    loop.close()
 
 
 @pytest.fixture(scope="session")
@@ -8,16 +19,16 @@ def env_settings():
     return load_dotenv(".env.test")
 
 
-@pytest.fixture(scope="session")
+@pytest_asyncio.fixture(scope="session")
 async def app(env_settings) -> FastAPI:
     pass
 
 
-@pytest.fixture(scope="session")
+@pytest_asyncio.fixture(scope="session")
 async def db(env_settings):
     pass
 
 
-@pytest.fixture(scope="session")
+@pytest_asyncio.fixture(scope="session")
 async def client(env_settings, db, app):
     pass
