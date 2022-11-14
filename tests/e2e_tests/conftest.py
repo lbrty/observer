1b15d4f579a3be7a6a31580fdc679f2f14c5a8a4
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-11-14 21:11:31 +0100

    Add pytest-asyncio

--- tests/e2e_tests/conftest.py
+++ tests/e2e_tests/conftest.py
@@ -1,9 +1,23 @@
+import pytest
 from fastapi import FastAPI
+from dotenv import load_dotenv
 
 
-async def app() -> FastAPI:
+@pytest.fixture(scope="session")
+def env_settings():
+    return load_dotenv(".env.test")
+
+
+@pytest.fixture(scope="session")
+async def app(env_settings) -> FastAPI:
     pass
 
 
-async def db():
+@pytest.fixture(scope="session")
+async def db(env_settings):
+    pass
+
+
+@pytest.fixture(scope="session")
+async def client(env_settings, db, app):
     pass
