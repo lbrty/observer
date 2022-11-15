e21ac9eaee50e4487145f6a8e9ce45b2e9864109
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-11-15 22:04:38 +0100

    Prepare conftest

--- tests/e2e_tests/conftest.py
+++ tests/e2e_tests/conftest.py
@@ -1,9 +1,16 @@
 import asyncio
+import os
 
 import pytest
 import pytest_asyncio
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from pydantic.networks import PostgresDsn
+
+from observer.app import create_app
+from observer.context import ctx
+from observer.db import connect, PoolOptions
+from observer.settings import db_settings, settings
 
 
 @pytest.fixture(scope="session")
@@ -16,19 +23,24 @@ def event_loop():
 
 @pytest.fixture(scope="session")
 def env_settings():
-    return load_dotenv(".env.test")
+    load_dotenv(".env.test")
+    settings.debug = True
+    db_settings.db_uri = PostgresDsn(os.getenv("DB_URI"), scheme="postgresql+asyncpg")
 
 
 @pytest_asyncio.fixture(scope="session")
 async def app(env_settings) -> FastAPI:
-    pass
+    return create_app(env_settings)
 
 
 @pytest_asyncio.fixture(scope="session")
 async def db(env_settings):
-    pass
+    ctx.db = await connect(
+        db_settings.db_uri,
+        PoolOptions()
+    )
 
 
 @pytest_asyncio.fixture(scope="session")
-async def client(env_settings, db, app):
+async def client(event_loop, env_settings, db, app):
     pass
