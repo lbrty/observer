996a466403948a6a3f019c563a9815205b2e44e8
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-11-18 23:10:55 +0100

    Create base exceptions

---
+++ observer/api/exceptions.py
@@ -0,0 +1,32 @@
+from enum import Enum, auto
+from typing import Any
+
+from starlette import status
+
+
+class ErrorCode(str, Enum):
+    unauthorized = auto()
+    internal_error = auto()
+
+
+class BaseAPIException(BaseException):
+    default_code: ErrorCode = ErrorCode.internal_error
+    default_message: str = "internal server error"
+    default_status: int = status.HTTP_500_INTERNAL_SERVER_ERROR
+    default_data: Any = None
+
+    def __init__(self, code: ErrorCode = None, status_code: int = None, message: str = None, data: Any = None):
+        self.default_code = code or self.default_code
+        self.default_status = status_code or self.default_status
+        self.default_message = message or self.default_message
+        self.data = data or self.default_data
+
+    def to_dict(self) -> dict:
+        return dict(
+            code=self.default_code.value, status_code=self.default_status, message=self.default_message, data=self.data
+        )
+
+
+class UnauthorizedError(BaseAPIException):
+    default_code = ErrorCode.unauthorized
+    default_status = status.HTTP_401_UNAUTHORIZED
