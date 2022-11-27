9c3167b5367148776ccc9740e19827ac71851b4a
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-11-27 21:48:59 +0100

    Fix pytest fixtures

--- tests/e2e_tests/conftest.py
+++ tests/e2e_tests/conftest.py
@@ -14,12 +14,15 @@ from cryptography.hazmat.primitives.serialization import (
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic.networks import PostgresDsn
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import NullPool
 
 from observer.app import create_app
 from observer.common.bcrypt import hash_password
 from observer.common.types import Role
 from observer.context import ctx
-from observer.db import PoolOptions, connect, disconnect, metadata
+from observer.db import Database, metadata
 from observer.entities.users import NewUser
 from observer.repositories.users import UsersRepository
 from observer.schemas.crypto import PrivateKey
@@ -41,67 +44,80 @@ def event_loop():
 def env_settings():
     load_dotenv(".env.test")
     settings.debug = True
-    print(os.getenv("DB_URI"))
     db_settings.db_uri = PostgresDsn(os.getenv("DB_URI"), scheme="postgresql+asyncpg")
     return settings
 
 
 @pytest.fixture(scope="session")
-async def encryption_keys(env_settings):
-    ctx.key_loader = FSLoader()
-    priv_key = generate_private_key(
-        public_exponent=settings.public_exponent,
-        key_size=2048,
+async def db_engine(env_settings):
+    opts = dict(
+        echo=False,
+        echo_pool=False,
+        isolation_level="AUTOCOMMIT",
+        poolclass=NullPool,
+    )
+    engine = create_async_engine(db_settings.db_uri, **opts)
+    yield engine
+    await engine.dispose()
+
+
+@pytest.fixture(scope="session")
+async def app_context(db_engine):
+    ctx.db = Database(
+        engine=db_engine,
+        session=sessionmaker(db_engine, class_=AsyncSession),
     )
 
-    priv_key_bytes = priv_key.private_bytes(
+    ctx.key_loader = FSLoader()
+    private_key = generate_private_key(
+        public_exponent=settings.public_exponent,
+        key_size=settings.key_size,
+    )
+
+    private_key_bytes = private_key.private_bytes(
         encoding=Encoding.PEM,
         format=PrivateFormat.PKCS8,
         encryption_algorithm=NoEncryption(),
     )
 
-    h = hashlib.new("sha256", priv_key_bytes)
+    h = hashlib.new("sha256", private_key_bytes)
     ctx.key_loader.keys = [
         PrivateKey(
             hash=str(h.hexdigest())[:16].upper(),
             key=load_pem_private_key(
-                priv_key_bytes,
+                private_key_bytes,
                 password=None,
             ),
         )
     ]
-    return ctx.key_loader
-
-
-@pytest.fixture(scope="session")
-async def db(env_settings):
-    ctx.db = await connect(db_settings.db_uri, PoolOptions())
-    async with ctx.db.engine.begin() as conn:
-        try:
-            await conn.run_sync(metadata.create_all)
-            yield ctx
-        finally:
-            await conn.run_sync(metadata.drop_all)
-            await disconnect(ctx.db.engine)
-
-
-@pytest.fixture(scope="session")
-def test_app(env_settings, db, encryption_keys) -> FastAPI:
     ctx.jwt_service = JWTService(ctx.key_loader.keys[0])
     ctx.users_repo = UsersRepository(ctx.db)
     ctx.users_service = UsersService(ctx.users_repo)
-    return create_app(env_settings)
+
+    yield ctx
 
 
 @pytest.fixture(scope="function")
+async def ensure_db(env_settings, db_engine):
+    async with db_engine.begin() as conn:
+        await conn.run_sync(metadata.drop_all)
+        await conn.run_sync(metadata.create_all)
+
+
+@pytest.fixture(scope="session")
+def test_app(env_settings) -> FastAPI:
+    return create_app(env_settings)
+
+
+@pytest.fixture(scope="session")
 async def client(test_app):
     app_client = httpx.AsyncClient(app=test_app, base_url="http://test")
     yield app_client
 
 
 @pytest.fixture(scope="function")
-async def consultant_user(db):
-    user = await ctx.users_service.repo.create_user(
+async def consultant_user(ensure_db, app_context):
+    user = await app_context.users_service.repo.create_user(
         NewUser(
             ref_id="ref-consultant-1",
             email="consultant-1@example.com",
@@ -112,4 +128,5 @@ async def consultant_user(db):
             is_confirmed=True,
         )
     )
+
     return user
