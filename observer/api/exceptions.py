ffcb47f5b1ac73d9600cb5dc7e6dd54ed12a841c
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-11-27 23:00:44 +0100

    Brush up exceptions

--- observer/api/exceptions.py
+++ observer/api/exceptions.py
@@ -10,21 +10,24 @@ class ErrorCode(str, Enum):
     internal_error = "internal_error"
 
 
-class BaseAPIException(BaseException):
+class BaseAPIException(Exception):
     default_code: ErrorCode = ErrorCode.internal_error
     default_message: str = "internal server error"
     default_status: int = status.HTTP_500_INTERNAL_SERVER_ERROR
     default_data: Any = None
 
     def __init__(self, code: ErrorCode = None, status_code: int = None, message: str = None, data: Any = None):
-        self.default_code = code or self.default_code
-        self.default_status = status_code or self.default_status
-        self.default_message = message or self.default_message
+        self.code = code or self.default_code
+        self.status = status_code or self.default_status
+        self.message = message or self.default_message
         self.data = data or self.default_data
 
     def to_dict(self) -> dict:
         return dict(
-            code=self.default_code.value, status_code=self.default_status, message=self.default_message, data=self.data
+            code=self.code.value,
+            status_code=self.status,
+            message=self.message,
+            data=self.data,
         )
 
 
