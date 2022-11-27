373e531ea79abe645994d4f4268d2ae7601f8fda
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-11-27 23:01:00 +0100

    Test login with invalid data

--- tests/e2e_tests/test_auth.py
+++ tests/e2e_tests/test_auth.py
@@ -1,6 +1,7 @@
 from starlette import status
 
 
+
 async def test_token_login(client, ensure_db, app_context, consultant_user):
     resp = await client.post(
         "/auth/token",
@@ -14,6 +15,23 @@ async def test_token_login(client, ensure_db, app_context, consultant_user):
     resp_json = resp.json()
     token_data, _ = await app_context.jwt_service.decode(resp_json["refresh_token"])
     assert token_data.ref_id == consultant_user.ref_id
+
+
+async def test_token_login_fails_if_credentials_are_wrong(client, ensure_db, consultant_user):
+    resp = await client.post(
+        "/auth/token",
+        json=dict(
+            email=consultant_user.email,
+            password="wronk passwort",
+        ),
+    )
+    assert resp.status_code == status.HTTP_401_UNAUTHORIZED
+    assert resp.json() == {
+        "code": "unauthorized",
+        "data": None,
+        "message": "Wrong email or password",
+        "status_code": 401,
+    }
 
 
 async def test_token_refresh(client, ensure_db, consultant_user):
