a11e5d8999f4c10be53444c0d332002e821328c1
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-11-28 23:18:09 +0100

    Re-use code

--- observer/cmd/swagger.py
+++ observer/cmd/swagger.py
@@ -1,10 +1,9 @@
 import json
 from pathlib import Path
 
-from fastapi import FastAPI
 from typer import Option, Typer
 
-from observer.api import auth, health
+from observer.app import create_app
 from observer.settings import settings
 
 swagger = Typer()
@@ -15,14 +14,7 @@ def generate(
     output_file: Path = Option(settings.swagger_output_file, help="Output file to save OpenAPI spec"),
 ):
     """Generate OpenAPI spec"""
-    app = FastAPI(
-        debug=settings.debug,
-        title=settings.title,
-        description=settings.description,
-        version="0.1.0",
-    )
-    app.include_router(auth.router)
-    app.include_router(health.router)
+    app = create_app(settings)
 
     if not output_file.parent.exists():
         output_file.parent.mkdir(exist_ok=True)
