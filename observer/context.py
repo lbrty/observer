5995fffa3eb200ebec5544f27bf22922233d15a1
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-12-04 22:49:49 +0100

    Add initial MFS schemas

--- observer/context.py
+++ observer/context.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from observer.db import Database
 from observer.repositories.users import UsersRepositoryInterface
 from observer.services.auth import AuthServiceInterface
-from observer.services.crypto import KeychainLoader, CryptoServiceInterface
+from observer.services.crypto import CryptoServiceInterface, KeychainLoader
 from observer.services.jwt import JWTService
 from observer.services.mfa import MFAServiceInterface
 from observer.services.users import UsersServiceInterface
---
+++ observer/schemas/otp.py
@@ -0,0 +1,14 @@
+from typing import List
+
+from pydantic import Field, SecretStr
+
+from observer.schemas.base import SchemaBase
+
+
+class MFAActivationRequest(SchemaBase):
+    secret: SecretStr = Field(..., description="TOTP secret")
+    totp_code: SecretStr = Field(..., description="TOTP code to validate and save for user")
+
+
+class MFAActivationResponse(SchemaBase):
+    backup_codes: List[str] = Field(..., description="List of backup codes")
