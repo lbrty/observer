948886849278f96d300447be9db3fffd6ed05c61
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-12-04 21:59:35 +0100

    Extend context object

--- observer/context.py
+++ observer/context.py
@@ -3,8 +3,9 @@ from dataclasses import dataclass
 from observer.db import Database
 from observer.repositories.users import UsersRepositoryInterface
 from observer.services.auth import AuthServiceInterface
-from observer.services.crypto import KeychainLoader
+from observer.services.crypto import KeychainLoader, CryptoServiceInterface
 from observer.services.jwt import JWTService
+from observer.services.mfa import MFAServiceInterface
 from observer.services.users import UsersServiceInterface
 
 
@@ -14,6 +15,8 @@ class Context:
     key_loader: KeychainLoader = None
     jwt_service: JWTService = None
     auth_service: AuthServiceInterface = None
+    crypto_service: CryptoServiceInterface = None
+    mfa_service: MFAServiceInterface = None
     users_repo: UsersRepositoryInterface = None
     users_service: UsersServiceInterface = None
 
