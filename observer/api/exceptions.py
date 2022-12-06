8692ecb3b0aea704e01d81fd53bd0c64c9d63dfe
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-12-06 22:03:03 +0100

    Add more api errors and add default messages

--- observer/api/exceptions.py
+++ observer/api/exceptions.py
@@ -7,6 +7,8 @@ from starlette import status
 class ErrorCode(str, Enum):
     unauthorized = "unauthorized"
     forbidden = "forbidden"
+    totp_error = "totp_error"
+    totp_exists_error = "totp_exists_error"
     registration_error = "registration_error"
     weak_password_error = "weak_password_error"
     internal_error = "internal_error"
@@ -33,21 +35,37 @@ class BaseAPIException(Exception):
         )
 
 
+class TOTPError(BaseAPIException):
+    default_code = ErrorCode.totp_error
+    default_status = status.HTTP_400_BAD_REQUEST
+    default_message = "invalid totp code"
+
+
+class TOTPExistsError(BaseAPIException):
+    default_code = ErrorCode.totp_exists_error
+    default_status = status.HTTP_400_BAD_REQUEST
+    default_message = "totp already configured"
+
+
 class UnauthorizedError(BaseAPIException):
     default_code = ErrorCode.unauthorized
     default_status = status.HTTP_401_UNAUTHORIZED
+    default_message = "authentication required"
 
 
 class ForbiddenError(BaseAPIException):
     default_code = ErrorCode.unauthorized
     default_status = status.HTTP_403_FORBIDDEN
+    default_message = "access forbidden"
 
 
 class RegistrationError(BaseAPIException):
     default_code = ErrorCode.registration_error
     default_status = status.HTTP_409_CONFLICT
+    default_message = "registration error"
 
 
 class WeakPasswordError(BaseAPIException):
     default_code = ErrorCode.weak_password_error
     default_status = status.HTTP_400_BAD_REQUEST
+    default_message = "password is weak"
