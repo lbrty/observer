92d5e743ef658199b6d8a755b7e15bc7125833e1
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-12-07 12:08:19 +0100

    Adjust tests

--- tests/e2e_tests/test_mfa.py
+++ tests/e2e_tests/test_mfa.py
@@ -1,27 +1,39 @@
 from starlette import status
 
+from tests.helpers.auth import get_auth_tokens
 
-async def test_mfa_setup_request(client, ensure_db, app_context, consultant_user):
-    resp = await client.post(
-        "/auth/token",
-        json=dict(
-            email=consultant_user.email,
-            password="secret",
-        ),
-    )
-    assert resp.status_code == status.HTTP_200_OK
 
-    resp_json = resp.json()
+async def test_mfa_configure_request(client, ensure_db, app_context, consultant_user):
+    auth_token = await get_auth_tokens(app_context, consultant_user)
     resp = await client.post(
         "/mfa/configure",
         json=dict(
             email=consultant_user.email,
             password="secret",
         ),
-        cookies=resp_json,
+        cookies=dict(
+            access_token=auth_token.access_token,
+            refresh_token=auth_token.refresh_token,
+        ),
     )
     assert resp.status_code == status.HTTP_200_OK
 
     resp_json = resp.json()
     assert "secret" in resp_json
     assert "qr_image" in resp_json
+
+
+async def test_mfa_setup_request(client, ensure_db, app_context, consultant_user):
+    auth_token = await get_auth_tokens(app_context, consultant_user)
+    resp = await client.post(
+        "/mfa/configure",
+        json=dict(
+            email=consultant_user.email,
+            password="secret",
+        ),
+        cookies=dict(
+            access_token=auth_token.access_token,
+            refresh_token=auth_token.refresh_token,
+        ),
+    )
+    assert resp.status_code == status.HTTP_200_OK
