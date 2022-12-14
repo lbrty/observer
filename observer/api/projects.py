49f36286b61a1ce4a8d71ef3270698cc4d80f143
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-12-14 14:42:08 +0100

    Create get project endpoint

--- observer/api/projects.py
+++ observer/api/projects.py
@@ -1,14 +1,13 @@
 from fastapi import APIRouter, Depends
 from starlette import status
 
-from observer.common.types import Identifier
-from observer.components.auth import current_user
-from observer.entities.users import User
+from observer.components.projects import project_details
+from observer.entities.projects import Project
 from observer.schemas.projects import ProjectResponse
 
 router = APIRouter(prefix="/projects")
 
 
 @router.get("/{project_id}", response_model=ProjectResponse, status_code=status.HTTP_200_OK)
-async def get_project(project_id: Identifier, user: User = Depends(current_user)) -> ProjectResponse:
-    pass
+async def get_project(project: Project = Depends(project_details)) -> ProjectResponse:
+    return ProjectResponse(**project.dict())
