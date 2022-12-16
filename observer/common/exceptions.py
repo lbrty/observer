5fa45e86f0e19fa9095e133cbc7b0e2ba25c3bdf
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-12-16 23:46:36 +0100

    Add additional responses helper

--- observer/common/exceptions.py
+++ observer/common/exceptions.py
@@ -1,7 +1,17 @@
+from typing import Any, Dict, List
+
 from fastapi.requests import Request
 from fastapi.responses import JSONResponse
+from pydantic.main import BaseModel
 
-from observer.api.exceptions import BaseAPIException
+from observer.api.exceptions import BaseAPIException, ErrorCode
+
+
+class APIError(BaseModel):
+    code: ErrorCode
+    status_code: int
+    message: str
+    data: Any
 
 
 async def handle_api_exception(_: Request, exc: BaseAPIException) -> JSONResponse:
@@ -9,3 +19,7 @@ async def handle_api_exception(_: Request, exc: BaseAPIException) -> JSONRespons
         exc.to_dict(),
         status_code=exc.status,
     )
+
+
+def get_api_errors(*status_codes: List[int]) -> Dict[int, Dict[str, APIError]]:
+    return {status_code: dict(model=APIError) for status_code in status_codes}
