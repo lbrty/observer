bcff829ca5c908055ecccb4095901ea4a090afb5
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-12-16 23:34:23 +0100

    Add initial project member schemas

--- observer/schemas/projects.py
+++ observer/schemas/projects.py
@@ -1,24 +1,28 @@
+from typing import List
+
 from pydantic import Field
 
-from observer.common.types import Identifier
+from observer.common.types import Identifier, SomeStr
 from observer.schemas.base import SchemaBase
+from observer.schemas.permissions import BasePermission
+from observer.schemas.users import UserResponse
 
 
 class ProjectFilters(SchemaBase):
     id: Identifier | None = Field(None, description="ID of project")
-    name: str | None = Field(None, description="Name of project")
-    description: str | None = Field(None, description="Description of project")
+    name: SomeStr = Field(None, description="Name of project")
+    description: SomeStr = Field(None, description="Description of project")
 
 
 class BaseProject(SchemaBase):
     name: str = Field(..., description="Name of project")
-    description: str | None = Field(None, description="Description of project")
+    description: SomeStr = Field(None, description="Description of project")
 
 
 class ProjectResponse(SchemaBase):
     id: Identifier = Field(..., description="ID of project")
     name: str = Field(..., description="Name of project")
-    description: str | None = Field(None, description="Description of project")
+    description: SomeStr = Field(None, description="Description of project")
 
 
 class NewProjectRequest(BaseProject):
@@ -31,4 +35,15 @@ class UpdateProjectRequest(BaseProject):
 
 class ProjectsResponse(SchemaBase):
     total: int = Field(..., description="Total count of projects")
-    items: list[ProjectResponse] = Field(..., description="List of projects")
+    items: List[ProjectResponse] = Field(..., description="List of projects")
+
+
+class ProjectMemberResponse(SchemaBase):
+    user: UserResponse
+    permissions: BasePermission
+
+
+# TODO: Add pagination
+class ProjectMembersResponse(SchemaBase):
+    # total: int = Field(..., description="Total count of members")
+    items: List[ProjectMemberResponse] = Field(..., description="List of project member")
