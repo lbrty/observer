e9429080d6de57bfffd1a690242076a2d7cff0e5
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-12-17 15:59:40 +0100

    Add pagination schema and dependency

---
+++ observer/components/pagination.py
@@ -0,0 +1,20 @@
+from fastapi import Query
+
+from observer.schemas.pagination import Pagination
+
+DEFAULT_LIMIT = 100
+MAX_LIMIT = 200
+DEFAULT_OFFSET = 0
+
+
+async def pagination(
+    limit: int = Query(DEFAULT_LIMIT, description="How many items to return?"),
+    offset: int = Query(DEFAULT_OFFSET, description="How many items to skip?"),
+) -> Pagination:
+    if limit < 0 or limit > MAX_LIMIT:
+        limit = DEFAULT_LIMIT
+
+    if offset < 0:
+        offset = DEFAULT_OFFSET
+
+    return Pagination(limit=limit, offset=offset)
