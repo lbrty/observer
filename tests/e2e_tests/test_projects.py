4300f212c9b27a5e752c54b06bdd441ac31b0416
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-12-18 21:37:51 +0100

    Add project add member test

--- tests/e2e_tests/test_projects.py
+++ tests/e2e_tests/test_projects.py
@@ -186,7 +186,7 @@ async def test_update_project_works_as_expected_for_users_without_permissions(
     assert resp.json() == {
         "code": "unauthorized",
         "status_code": 403,
-        "message": "Yoo can not view this project",
+        "message": "You can not update this project",
         "data": None,
     }
 
@@ -219,7 +219,78 @@ async def test_get_project_members_works_as_expected(authorized_client, ensure_d
                     "can_create_projects": True,
                     "can_read_documents": True,
                     "can_read_personal_info": True,
+                    "can_invite_members": True,
                 },
             }
         ],
+    }
+
+
+async def test_add_project_member_works_as_expected(
+    authorized_client, ensure_db, app_context, consultant_user, guest_user
+):
+    resp = await authorized_client.post(
+        "/projects/",
+        json=dict(
+            name="Test Project",
+            description="Project description",
+        ),
+    )
+    assert resp.status_code == status.HTTP_201_CREATED
+    resp_json = resp.json()
+    project_id = resp_json["id"]
+    resp = await authorized_client.post(
+        f"/projects/{project_id}/members",
+        json=dict(
+            can_create=False,
+            can_read=True,
+            can_update=True,
+            can_delete=False,
+            can_create_projects=False,
+            can_read_documents=False,
+            can_read_personal_info=False,
+            can_invite_members=False,
+            project_id=project_id,
+            user_id=str(guest_user.id),
+        ),
+    )
+    assert resp.status_code == status.HTTP_200_OK
+
+    resp = await authorized_client.get(f"/projects/{project_id}/members")
+    assert resp.status_code == status.HTTP_200_OK
+    assert resp.json() == {
+        "items": [
+            {
+                "ref_id": "ref-consultant-1",
+                "is_active": True,
+                "full_name": "full name",
+                "role": "consultant",
+                "permissions": {
+                    "can_create": True,
+                    "can_read": True,
+                    "can_update": True,
+                    "can_delete": True,
+                    "can_create_projects": True,
+                    "can_read_documents": True,
+                    "can_read_personal_info": True,
+                    "can_invite_members": True,
+                },
+            },
+            {
+                "ref_id": "ref-guest-1",
+                "is_active": True,
+                "full_name": "full name",
+                "role": "guest",
+                "permissions": {
+                    "can_create": False,
+                    "can_read": True,
+                    "can_update": True,
+                    "can_delete": False,
+                    "can_create_projects": False,
+                    "can_read_documents": False,
+                    "can_read_personal_info": False,
+                    "can_invite_members": False,
+                },
+            },
+        ]
     }
