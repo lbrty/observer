3a68e59a8c562b60ace76675dff378cc1bdd9bd4
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-12-18 21:32:23 +0100

    Add can_invite_members to permission schema

--- observer/schemas/permissions.py
+++ observer/schemas/permissions.py
@@ -12,6 +12,7 @@ class BasePermission(SchemaBase):
     can_create_projects: bool = Field(False, description="User can create projects")
     can_read_documents: bool = Field(False, description="User can read documents")
     can_read_personal_info: bool = Field(False, description="User can read personal info")
+    can_invite_members: bool = Field(False, description="User can invite new members to projects")
 
 
 class PermissionResponse(BasePermission):
