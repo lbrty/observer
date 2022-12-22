2e53997a00288e067a6512f2a5e0181ae0d6c434
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-12-22 11:22:45 +0100

    Implement state crud actions

--- observer/repositories/world.py
+++ observer/repositories/world.py
@@ -4,25 +4,68 @@ from sqlalchemy import delete, insert, select, update
 
 from observer.common.types import Identifier
 from observer.db import Database
-from observer.db.tables.world import countries
-from observer.entities.base import SomeCountry
-from observer.entities.world import Country, NewCountry, UpdateCountry
+from observer.db.tables.world import countries, places, states
+from observer.entities.base import SomeCountry, SomePlace, SomeState
+from observer.entities.world import (
+    Country,
+    NewCountry,
+    NewPlace,
+    NewState,
+    Place,
+    State,
+    UpdateCountry,
+    UpdatePlace,
+    UpdateState,
+)
 
 
 class WorldRepositoryInterface(Protocol):
     async def create_country(self, new_country: NewCountry) -> Country:
         raise NotImplementedError
 
+    # Countries
     async def get_countries(self) -> List[Country]:
         raise NotImplementedError
 
-    async def get_country(self, country_id: Identifier) -> Country:
+    async def get_country(self, country_id: Identifier) -> SomeCountry:
         raise NotImplementedError
 
-    async def update_country(self, country_id: Identifier, updates: UpdateCountry) -> Country:
+    async def update_country(self, country_id: Identifier, updates: UpdateCountry) -> SomeCountry:
         raise NotImplementedError
 
-    async def delete_country(self, country_id: Identifier) -> Country:
+    async def delete_country(self, country_id: Identifier) -> SomeCountry:
+        raise NotImplementedError
+
+    # States
+    async def create_state(self, new_state: NewState) -> State:
+        raise NotImplementedError
+
+    async def get_states(self) -> List[State]:
+        raise NotImplementedError
+
+    async def get_state(self, country_id: Identifier) -> SomeState:
+        raise NotImplementedError
+
+    async def update_state(self, country_id: Identifier, updates: UpdateState) -> SomeState:
+        raise NotImplementedError
+
+    async def delete_state(self, country_id: Identifier) -> SomeState:
+        raise NotImplementedError
+
+    # Places
+    async def create_place(self, new_place: NewPlace) -> Place:
+        raise NotImplementedError
+
+    async def get_places(self) -> List[Place]:
+        raise NotImplementedError
+
+    async def get_place(self, place_id: Identifier) -> SomePlace:
+        raise NotImplementedError
+
+    async def update_place(self, place_id: Identifier, updates: UpdatePlace) -> SomePlace:
+        raise NotImplementedError
+
+    async def delete_place(self, place_id: Identifier) -> SomePlace:
         raise NotImplementedError
 
 
@@ -30,6 +73,7 @@ class WorldRepository(WorldRepositoryInterface):
     def __init__(self, db: Database):
         self.db = db
 
+    # Countries
     async def create_country(self, new_country: NewCountry) -> Country:
         query = insert(countries).values(**new_country.dict()).returning("*")
         result = await self.db.fetchone(query)
@@ -58,5 +102,69 @@ class WorldRepository(WorldRepositoryInterface):
         query = delete(countries).where(countries.c.id == country_id).returning("*")
         if row := await self.db.fetchone(query):
             return Country(**row)
+
+        return None
+
+    # States
+    async def create_state(self, new_state: NewState) -> State:
+        query = insert(states).values(**new_state.dict()).returning("*")
+        result = await self.db.fetchone(query)
+        return State(**result)
+
+    async def get_states(self) -> List[State]:
+        query = select(states)
+        rows = await self.db.fetchall(query)
+        return [State(**row) for row in rows]
+
+    async def get_state(self, state_id: Identifier) -> SomeState:
+        query = select(states).where(states.c.id == state_id)
+        if row := await self.db.fetchone(query):
+            return State(**row)
+
+        return None
+
+    async def update_state(self, state_id: Identifier, updates: UpdateState) -> SomeState:
+        query = update(states).values(**updates.dict()).where(states.c.id == state_id).returning("*")
+        if row := await self.db.fetchone(query):
+            return State(**row)
+
+        return None
+
+    async def delete_state(self, state_id: Identifier) -> SomeState:
+        query = delete(states).where(states.c.id == state_id).returning("*")
+        if row := await self.db.fetchone(query):
+            return State(**row)
+
+        return None
+
+    # Places
+    async def create_place(self, new_place: NewPlace) -> Place:
+        query = insert(places).values(**new_place.dict()).returning("*")
+        result = await self.db.fetchone(query)
+        return Place(**result)
+
+    async def get_places(self) -> List[Place]:
+        query = select(places)
+        rows = await self.db.fetchall(query)
+        return [Place(**row) for row in rows]
+
+    async def get_place(self, place_id: Identifier) -> SomePlace:
+        query = select(places).where(places.c.id == place_id)
+        if row := await self.db.fetchone(query):
+            return Place(**row)
+
+        return None
+
+    async def update_place(self, place_id: Identifier, updates: UpdatePlace) -> SomePlace:
+        query = update(places).values(**updates.dict()).where(places.c.id == place_id).returning("*")
+        if row := await self.db.fetchone(query):
+            return Place(**row)
+
+        return None
+
+    async def delete_place(self, place_id: Identifier) -> SomePlace:
+        query = delete(places).where(places.c.id == place_id).returning("*")
+        if row := await self.db.fetchone(query):
+            return Place(**row)
 
         return None
--- observer/services/world.py
+++ observer/services/world.py
@@ -3,13 +3,24 @@ from typing import List, Protocol
 
 from observer.api.exceptions import NotFoundError
 from observer.common.types import Identifier
-from observer.entities.world import Country, NewCountry, UpdateCountry
+from observer.entities.base import SomeCountry, SomeState
+from observer.entities.world import (
+    Country,
+    NewCountry,
+    NewState,
+    State,
+    UpdateCountry,
+    UpdateState,
+)
 from observer.repositories.world import WorldRepositoryInterface
 from observer.schemas.audit_logs import NewAuditLog
 from observer.schemas.world import (
     CountryResponse,
     NewCountryRequest,
+    NewStateRequest,
+    StateResponse,
     UpdateCountryRequest,
+    UpdateStateRequest,
 )
 
 
@@ -17,22 +28,20 @@ class WorldServiceInterface(Protocol):
     tag: str
     repo: WorldRepositoryInterface
 
+    # Countries
     async def create_country(self, new_country: NewCountryRequest) -> Country:
         raise NotImplementedError
 
     async def get_countries(self) -> List[Country]:
         raise NotImplementedError
 
-    async def get_country(self, country_id: Identifier) -> Country:
+    async def get_country(self, country_id: Identifier) -> SomeCountry:
         raise NotImplementedError
 
-    async def update_country(self, country_id: Identifier, updates: UpdateCountryRequest) -> Country:
+    async def update_country(self, country_id: Identifier, updates: UpdateCountryRequest) -> SomeCountry:
         raise NotImplementedError
 
-    async def delete_country(self, country_id: Identifier) -> Country:
-        raise NotImplementedError
-
-    async def create_log(self, ref: str, expires_in: timedelta | None, data: dict | None = None) -> NewAuditLog:
+    async def delete_country(self, country_id: Identifier) -> SomeCountry:
         raise NotImplementedError
 
     @staticmethod
@@ -43,13 +52,41 @@ class WorldServiceInterface(Protocol):
     async def countries_to_response(country_list: List[Country]) -> List[CountryResponse]:
         raise NotImplementedError
 
+    # States
+    async def create_state(self, new_state: NewStateRequest) -> State:
+        raise NotImplementedError
+
+    async def get_states(self) -> List[State]:
+        raise NotImplementedError
+
+    async def get_state(self, state_id: Identifier) -> SomeState:
+        raise NotImplementedError
+
+    async def update_state(self, state_id: Identifier, updates: UpdateStateRequest) -> SomeState:
+        raise NotImplementedError
+
+    async def delete_state(self, state_id: Identifier) -> SomeState:
+        raise NotImplementedError
+
+    @staticmethod
+    async def state_to_response(state: State) -> StateResponse:
+        raise NotImplementedError
+
+    @staticmethod
+    async def states_to_response(state_list: List[State]) -> List[StateResponse]:
+        raise NotImplementedError
+
+    async def create_log(self, ref: str, expires_in: timedelta | None, data: dict | None = None) -> NewAuditLog:
+        raise NotImplementedError
+
 
 class WorldService(WorldServiceInterface):
-    tag: str = "source=service:places"
+    tag: str = "source=service:world"
 
     def __init__(self, places_repository: WorldRepositoryInterface):
         self.repo = places_repository
 
+    # States
     async def create_country(self, new_country: NewCountryRequest) -> Country:
         return await self.repo.create_country(NewCountry(**new_country.dict()))
 
@@ -68,6 +105,41 @@ class WorldService(WorldServiceInterface):
     async def delete_country(self, country_id: Identifier) -> Country:
         return await self.repo.delete_country(country_id)
 
+    @staticmethod
+    async def country_to_response(country: Country) -> CountryResponse:
+        return CountryResponse(**country.dict())
+
+    @staticmethod
+    async def countries_to_response(country_list: List[Country]) -> List[CountryResponse]:
+        return [CountryResponse(**country.dict()) for country in country_list]
+
+    # States
+    async def create_state(self, new_state: NewStateRequest) -> State:
+        return await self.repo.create_state(NewState(**new_state.dict()))
+
+    async def get_states(self) -> List[State]:
+        return await self.repo.get_states()
+
+    async def get_state(self, state_id: Identifier) -> SomeState:
+        if state := await self.repo.get_state(state_id):
+            return state
+
+        raise NotFoundError(message="State not found")
+
+    async def update_state(self, state_id: Identifier, updates: UpdateStateRequest) -> SomeState:
+        return await self.repo.update_state(state_id, UpdateState(**updates.dict()))
+
+    async def delete_state(self, state_id: Identifier) -> SomeState:
+        return await self.repo.delete_state(state_id)
+
+    @staticmethod
+    async def state_to_response(state: State) -> StateResponse:
+        return StateResponse(**state.dict())
+
+    @staticmethod
+    async def states_to_response(state_list: List[State]) -> List[StateResponse]:
+        return [StateResponse(**state.dict()) for state in state_list]
+
     async def create_log(self, ref: str, expires_in: timedelta | None, data: dict | None = None) -> NewAuditLog:
         now = datetime.now(tz=timezone.utc)
         expires_at = None
@@ -79,11 +151,3 @@ class WorldService(WorldServiceInterface):
             data=data,
             expires_at=expires_at,
         )
-
-    @staticmethod
-    async def country_to_response(country: Country) -> CountryResponse:
-        return CountryResponse(**country.dict())
-
-    @staticmethod
-    async def countries_to_response(country_list: List[Country]) -> List[CountryResponse]:
-        return [CountryResponse(**country.dict()) for country in country_list]
