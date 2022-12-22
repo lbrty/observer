52f3f63403c23c035681c898832e666a7273e213
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-12-22 10:19:08 +0100

    Add more tests to countries endpoint

--- tests/e2e_tests/test_world.py
+++ tests/e2e_tests/test_world.py
@@ -41,3 +41,83 @@ async def test_get_countries_works_as_expected(
     country = resp.json()[0]
     assert country["name"] == "Qyrgyzstan"
     assert country["code"] == "qy"
+
+
+async def test_get_country_works_as_expected(
+    authorized_client,
+    ensure_db,
+    app_context,
+    consultant_user,
+):
+    resp = await authorized_client.post(
+        "/world/countries",
+        json=dict(
+            name="Qyrgyzstan",
+            code="qy",
+        ),
+    )
+    assert resp.status_code == status.HTTP_201_CREATED
+    country_id = resp.json()["id"]
+    resp = await authorized_client.get(f"/world/countries/{country_id}")
+    assert resp.status_code == status.HTTP_200_OK
+    country = resp.json()
+    assert country["name"] == "Qyrgyzstan"
+    assert country["code"] == "qy"
+
+
+async def test_update_country_works_as_expected(
+    authorized_client,
+    ensure_db,
+    app_context,
+    consultant_user,
+):
+    resp = await authorized_client.post(
+        "/world/countries",
+        json=dict(
+            name="Qyrgyzstan",
+            code="qy",
+        ),
+    )
+    assert resp.status_code == status.HTTP_201_CREATED
+    country_id = resp.json()["id"]
+    resp = await authorized_client.put(
+        f"/world/countries/{country_id}",
+        json=dict(
+            name="Qyrgyz Ordosu",
+            code="qy",
+        ),
+    )
+    assert resp.status_code == status.HTTP_200_OK
+
+    resp = await authorized_client.get(f"/world/countries/{country_id}")
+    assert resp.status_code == status.HTTP_200_OK
+    country = resp.json()
+    assert country["name"] == "Qyrgyz Ordosu"
+    assert country["code"] == "qy"
+
+
+async def test_delete_country_works_as_expected(
+    authorized_client,
+    ensure_db,
+    app_context,
+    consultant_user,
+):
+    countries = []
+    for n in range(10):
+        resp = await authorized_client.post(
+            "/world/countries",
+            json=dict(
+                name=f"Qyrgyzstan {n}",
+                code=f"qy{n}",
+            ),
+        )
+        assert resp.status_code == status.HTTP_201_CREATED
+        countries.append(resp.json())
+    assert len(countries) == 10
+
+    await authorized_client.delete(f"/world/countries/{countries[0]['id']}")
+    await authorized_client.delete(f"/world/countries/{countries[1]['id']}")
+
+    resp = await authorized_client.get("/world/countries")
+    assert resp.status_code == status.HTTP_200_OK
+    assert len(resp.json()) == 8
