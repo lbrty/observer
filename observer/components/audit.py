b07a7ef87cde0b3e048cfc22eab76e1196b27f71
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-12-23 16:40:18 +0100

    Implement trackable properties

---
+++ observer/components/audit.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timezone
+from typing import Any
+
+from box import Box
+from fastapi.encoders import jsonable_encoder
+
+from observer.schemas.audit_logs import NewAuditLog
+
+
+class Props:
+    def __init__(self, data: Box):
+        self.data = data
+
+    def new_event(self, refs: str, data: Any) -> NewAuditLog:
+        now = datetime.now(tz=timezone.utc)
+        expires_at = None
+        if self.data.expires_in:
+            expires_at = now + self.data.expires_in
+
+        return NewAuditLog(
+            ref=f"{self.data.tag},{refs}",
+            data=jsonable_encoder(data) if data else None,
+            expires_at=expires_at,
+        )
+
+
+class Tracked:
+    def __init__(self, **data: Any):
+        self.tracker = Props(Box(data))
+
+    async def __call__(self) -> Props:
+        return self.tracker
