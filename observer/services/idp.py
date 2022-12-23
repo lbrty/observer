c8270a5bc27037540c65c921854706a2d7e0bb40
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-12-23 14:07:36 +0100

    Cleanup code

--- observer/services/idp.py
+++ observer/services/idp.py
@@ -1,33 +1,13 @@
 from datetime import datetime, timedelta, timezone
-from typing import List, Protocol
+from typing import Protocol
 
-from observer.api.exceptions import NotFoundError
-from observer.common.types import Identifier, SomeStr
-from observer.entities.displaced_persons import Category, NewCategory, UpdateCategory
 from observer.repositories.idp import IDPRepositoryInterface
 from observer.schemas.audit_logs import NewAuditLog
-from observer.schemas.displaced_persons import NewCategoryRequest, UpdateCategoryRequest
 
 
 class IDPServiceInterface(Protocol):
     tag: str
     repo: IDPRepositoryInterface
-
-    # Categories
-    async def create_category(self, new_category: NewCategoryRequest) -> Category:
-        raise NotImplementedError
-
-    async def get_categories(self, name: SomeStr = None) -> List[Category]:
-        raise NotImplementedError
-
-    async def get_category(self, category_id: Identifier) -> Category:
-        raise NotImplementedError
-
-    async def update_category(self, category_id: Identifier, updates: UpdateCategoryRequest) -> Category:
-        raise NotImplementedError
-
-    async def delete_category(self, category_id: Identifier) -> Category:
-        raise NotImplementedError
 
     async def create_log(self, ref: str, expires_in: timedelta | None, data: dict | None = None) -> NewAuditLog:
         raise NotImplementedError
@@ -38,27 +18,6 @@ class IDPService(IDPServiceInterface):
 
     def __init__(self, idp_repository: IDPRepositoryInterface):
         self.repo = idp_repository
-
-    # Categories
-    async def create_category(self, new_category: NewCategoryRequest) -> Category:
-        return await self.repo.create_category(NewCategory(**new_category.dict()))
-
-    async def get_categories(self, name: SomeStr = None) -> List[Category]:
-        return await self.repo.get_categories(name)
-
-    async def get_category(self, category_id: Identifier) -> Category:
-        if category := await self.repo.get_category(category_id):
-            return category
-
-        raise NotFoundError(message="Category not found")
-
-    async def update_category(self, category_id: Identifier, updates: UpdateCategoryRequest) -> Category:
-        await self.get_category(category_id)
-        return await self.repo.update_category(category_id, UpdateCategory(**updates.dict()))
-
-    async def delete_category(self, category_id: Identifier) -> Category:
-        await self.get_category(category_id)
-        return await self.repo.delete_category(category_id)
 
     async def create_log(self, ref: str, expires_in: timedelta | None, data: dict | None = None) -> NewAuditLog:
         now = datetime.now(tz=timezone.utc)
