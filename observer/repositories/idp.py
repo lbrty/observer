d6a4d6bf871f65af6eefdf7f9675f8498a5b621a
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-12-24 21:58:40 +0100

    Implement create id and get idp methods

--- observer/repositories/idp.py
+++ observer/repositories/idp.py
@@ -1,14 +1,19 @@
 from typing import Protocol
 
-from sqlalchemy import insert
+from sqlalchemy import insert, select
 
+from observer.common.types import Identifier
 from observer.db import Database
 from observer.db.tables.idp import people
+from observer.entities.base import SomeIDP
 from observer.entities.idp import IDP, NewIDP
 
 
 class IDPRepositoryInterface(Protocol):
     async def create_idp(self, new_idp: NewIDP) -> IDP:
+        raise NotImplementedError
+
+    async def get_idp(self, idp_id: Identifier) -> SomeIDP:
         raise NotImplementedError
 
 
@@ -20,3 +25,10 @@ class IDPRepository(IDPRepositoryInterface):
         query = insert(people).values(**new_idp.dict()).returning("*")
         result = await self.db.fetchone(query)
         return IDP(**result)
+
+    async def get_idp(self, idp_id: Identifier) -> SomeIDP:
+        query = select(people).where(people.c.id == idp_id)
+        if result := await self.db.fetchone(query):
+            return IDP(**result)
+
+        return None
--- observer/services/idp.py
+++ observer/services/idp.py
@@ -1,9 +1,14 @@
 from typing import Protocol
 
+from observer.api.exceptions import NotFoundError
+from observer.common.types import Identifier
 from observer.entities.idp import IDP, NewIDP
 from observer.repositories.idp import IDPRepositoryInterface
 from observer.schemas.idp import NewIDPRequest
+from observer.services.categories import CategoryServiceInterface
 from observer.services.crypto import CryptoServiceInterface
+from observer.services.projects import ProjectsServiceInterface
+from observer.services.world import WorldServiceInterface
 
 
 class IDPServiceInterface(Protocol):
@@ -14,15 +19,64 @@ class IDPServiceInterface(Protocol):
     async def create_idp(self, new_idp: NewIDPRequest) -> IDP:
         raise NotImplementedError
 
+    async def get_idp(self, idp_id: Identifier) -> IDP:
+        raise NotImplementedError
+
 
 class IDPService(IDPServiceInterface):
     tag: str = "source=service:idp"
 
-    def __init__(self, idp_repository: IDPRepositoryInterface, crypto_service: CryptoServiceInterface):
+    def __init__(
+        self,
+        idp_repository: IDPRepositoryInterface,
+        crypto_service: CryptoServiceInterface,
+        categories: CategoryServiceInterface,
+        projects: ProjectsServiceInterface,
+        world: WorldServiceInterface,
+    ):
         self.repo = idp_repository
         self.crypto_service = crypto_service
+        self.categories_service = categories
+        self.projects_service = projects
+        self.world_service = world
 
     async def create_idp(self, new_idp: NewIDPRequest) -> IDP:
-        # TODO: Generate RSA key and assign as encryption key
-        #       Then encrypt fields we need to encrypt
-        return await self.repo.create_idp(NewIDP(**new_idp.dict()))
+        new_idp = NewIDP(**new_idp.dict())
+        key_hash = self.crypto_service.keychain.keys[0].hash
+        if new_idp.project_id:
+            await self.projects_service.get_by_id(new_idp.project_id)
+
+        if new_idp.current_place_id:
+            await self.world_service.get_place(new_idp.current_place_id)
+
+        if new_idp.from_place_id:
+            await self.world_service.get_place(new_idp.from_place_id)
+
+        if new_idp.email:
+            encrypted_email = await self.crypto_service.encrypt(
+                key_hash,
+                bytes(new_idp.email),
+            )
+            new_idp.email = f"{key_hash}:{encrypted_email.decode()}"
+
+        if new_idp.phone_number and new_idp.phone_number.strip():
+            encrypted_phone_number = await self.crypto_service.encrypt(
+                key_hash,
+                bytes(new_idp.phone_number),
+            )
+            new_idp.phone_number = f"{key_hash}:{encrypted_phone_number}"
+
+        if new_idp.phone_number_additional and new_idp.phone_number_additional.strip():
+            encrypted_phone_number = await self.crypto_service.encrypt(
+                key_hash,
+                bytes(new_idp.phone_number_additional),
+            )
+            new_idp.phone_number_additional = f"{key_hash}:{encrypted_phone_number}"
+
+        return await self.repo.create_idp(new_idp)
+
+    async def get_idp(self, idp_id: Identifier) -> IDP:
+        if idp := await self.repo.get_idp(idp_id):
+            return idp
+
+        raise NotFoundError(message="IDP record not found")
