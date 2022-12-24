72dd17ddc1c98d148d0ecfb7fc32e0c348ea9ce4
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-12-24 21:57:07 +0100

    Implement aes encryption methods

--- observer/services/crypto.py
+++ observer/services/crypto.py
@@ -1,7 +1,11 @@
 import base64
+import os
+from dataclasses import dataclass
 from typing import Protocol
 
+import shortuuid
 from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives.hashes import SHA256
 from structlog import get_logger
 
@@ -13,8 +17,15 @@ from observer.services.keys import Keychain
 logger = get_logger(service="crypto")
 
 
+@dataclass
+class AESCipherOptions:
+    secret: bytes
+    iv: bytes
+
+
 class CryptoServiceInterface(Protocol):
     keychain: Keychain | None
+    padding: padding.OAEP | None
 
     async def encrypt(self, key_hash: SomeStr, data: bytes) -> bytes:
         raise NotImplementedError
@@ -22,10 +33,16 @@ class CryptoServiceInterface(Protocol):
     async def decrypt(self, key_hash: SomeStr, data: bytes) -> bytes:
         raise NotImplementedError
 
-    async def aes_encrypt(self, secret: str, data: bytes) -> bytes:
+    async def aes_cipher_options(self, key_bits: int) -> AESCipherOptions:
         raise NotImplementedError
 
-    async def aes_decrypt(self, secret: str, data: bytes) -> bytes:
+    async def gen_key(self, key_bits: int) -> bytes:
+        raise NotImplementedError
+
+    async def aes_encrypt(self, secret: bytes, iv: bytes, data: bytes) -> bytes:
+        raise NotImplementedError
+
+    async def aes_decrypt(self, secret: bytes, iv: bytes, ciphertext: bytes) -> bytes:
         raise NotImplementedError
 
 
@@ -57,8 +74,35 @@ class CryptoService(CryptoServiceInterface):
 
         raise InternalError(message=f"private key with hash={key_hash} not found")
 
-    async def aes_encrypt(self, secret: str, data: bytes) -> bytes:
-        ...
+    # AES encryption and decryption is based on official docs and recommendations
+    # https://cryptography.io/en/latest/hazmat/primitives/symmetric-encryption/
+    async def aes_cipher_options(self, key_bits: int) -> AESCipherOptions:
+        secret = await self.gen_key(key_bits)
+        # 96-bit IV values can be processed more efficiently, so is recommended
+        # for situations in which efficiency is more desired.
+        # See for more https://crypto.stackexchange.com/questions/41601/aes-gcm-recommended-iv-size-why-12-bytes
+        iv = await self.gen_key(12)
+        return AESCipherOptions(
+            secret=secret,
+            iv=iv,
+        )
 
-    async def aes_decrypt(self, secret: str, data: bytes) -> bytes:
-        ...
+    async def gen_key(self, key_bits: int) -> bytes:
+        random_bytes = base64.b64encode(os.urandom(key_bits))
+        return bytes(shortuuid.uuid(name=random_bytes.decode()))
+
+    async def aes_encrypt(self, secret: bytes, iv: bytes, data: bytes) -> bytes:
+        cipher = Cipher(
+            algorithms.AES(key=secret),
+            modes.GCM(iv),
+        )
+        encryptor = cipher.encryptor()
+        return encryptor.update(data) + encryptor.finalize()
+
+    async def aes_decrypt(self, secret: bytes, iv: bytes, ciphertext: bytes) -> bytes:
+        cipher = Cipher(
+            algorithms.AES(key=secret),
+            modes.GCM(iv),
+        )
+        decryptor = cipher.decryptor()
+        return decryptor.update(ciphertext) + decryptor.finalize()
