1a18c7ec9f84e68fd737d2387e77c0acdf8d09dc
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-12-25 10:24:44 +0100

    Create crud test helpers

--- tests/helpers/crud.py
+++ tests/helpers/crud.py
@@ -1,7 +1,100 @@
+from observer.common.types import Identifier, PlaceType
 from observer.context import Context
+from observer.entities.idp import Category, NewCategory
 from observer.entities.projects import NewProject, Project
+from observer.entities.world import (
+    Country,
+    NewCountry,
+    NewPlace,
+    NewState,
+    Place,
+    State,
+)
 
 
 async def create_project(ctx: Context, name: str, description: str) -> Project:
     project = await ctx.projects_repo.create_project(NewProject(name=name, description=description))
     return project
+
+
+async def create_category(ctx: Context, name: str) -> Category:
+    category = await ctx.category_repo.create_category(NewCategory(name=name))
+    return category
+
+
+async def create_country(ctx: Context, name: str, code: str) -> Country:
+    country = await ctx.world_repo.create_country(NewCountry(name=name, code=code))
+    return country
+
+
+async def create_state(
+    ctx: Context,
+    name: str,
+    code: str,
+    country_id: Identifier,
+) -> State:
+    state = await ctx.world_repo.create_state(
+        NewState(
+            name=name,
+            code=code,
+            country_id=country_id,
+        )
+    )
+    return state
+
+
+async def create_city(
+    ctx: Context,
+    name: str,
+    code: str,
+    country_id: Identifier,
+    state_id: Identifier,
+) -> Place:
+    city = await ctx.world_repo.create_place(
+        NewPlace(
+            name=name,
+            code=code,
+            place_type=PlaceType.city,
+            country_id=country_id,
+            state_id=state_id,
+        )
+    )
+    return city
+
+
+async def create_town(
+    ctx: Context,
+    name: str,
+    code: str,
+    country_id: Identifier,
+    state_id: Identifier,
+) -> Place:
+    town = await ctx.world_repo.create_place(
+        NewPlace(
+            name=name,
+            code=code,
+            place_type=PlaceType.town,
+            country_id=country_id,
+            state_id=state_id,
+        )
+    )
+    return town
+
+
+async def create_village(
+    ctx: Context,
+    name: str,
+    code: str,
+    country_id: Identifier,
+    state_id: Identifier,
+) -> Place:
+    village = await ctx.world_repo.create_place(
+        NewPlace(
+            name=name,
+            code=code,
+            place_type=PlaceType.village,
+            country_id=country_id,
+            state_id=state_id,
+        )
+    )
+    return village
