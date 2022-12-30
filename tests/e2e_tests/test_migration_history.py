f7cb533d20c3ca7bc8dc9ee39a47bf856e60c360
Sultan Iman <sultan.imanhodjaev@gmail.com>
2022-12-30 17:02:57 +0100

    Prepare a test file for migration history

---
+++ tests/e2e_tests/test_migration_history.py
@@ -0,0 +1,10 @@
+async def test_add_migration_history_works_as_expected():
+    pass
+
+
+async def test_get_migration_history_works_as_expected():
+    pass
+
+
+async def test_delete_migration_history_works_as_expected():
+    pass
