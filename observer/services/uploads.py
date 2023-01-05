dac3f0cc272808c7f675d52a86df1f0cc2075a4c
Sultan Iman <sultan.imanhodjaev@gmail.com>
2023-01-05 20:45:03 +0100

    Adjust upload handler validations

--- observer/services/uploads.py
+++ observer/services/uploads.py
@@ -1,23 +1,16 @@
 import base64
 from dataclasses import dataclass
-from typing import Optional
 
 from fastapi import UploadFile
 
-from observer.api.exceptions import (
-    ConflictError,
-    ContentLengthRequiredError,
-    TooLargeDocumentError,
-    UnsupportedDocumentError,
-)
+from observer.api.exceptions import TooLargeDocumentError, UnsupportedDocumentError
 from observer.entities.documents import AllowedDocumentTypes
-from observer.services.crypto import AESCipherOptions, ICryptoService
+from observer.services.crypto import ICryptoService
 from observer.settings import settings
 
 
 @dataclass
 class FileVault:
-    aes_options: AESCipherOptions
     key_hash: str
     encryption_key: str
     encrypted_file: bytes
@@ -27,7 +20,7 @@ class UploadHandler:
     def __init__(self, crypto: ICryptoService):
         self.crypto = crypto
 
-    async def process_upload(self, content_length: Optional[int], file: UploadFile) -> FileVault:
+    async def process_upload(self, file: UploadFile) -> FileVault:
         """Validate and encrypt uploaded file.
 
         To encrypt data we do the following things
@@ -38,18 +31,12 @@ class UploadHandler:
             4. B64 encode AES secrets pair `secret:iv` and encrypted file contents,
             5. B64 encode secrets and encrypted contents in the format `B64(secret:iv):B64(encrypted contents)`
         """
-        if not content_length:
-            raise ContentLengthRequiredError
-
-        if content_length > settings.max_upload_size:
-            raise TooLargeDocumentError
-
         if file.content_type not in AllowedDocumentTypes:
             raise UnsupportedDocumentError
 
         contents = await file.read()
-        if len(contents) != content_length:
-            raise ConflictError(message="Content-Length and actual content size mismatch")
+        if len(contents) > settings.max_upload_size:
+            raise TooLargeDocumentError
 
         seal_options = await self.crypto.aes_cipher_options(settings.aes_key_bits)
         encrypted_contents = base64.b64encode(
@@ -66,7 +53,6 @@ class UploadHandler:
         encrypted_secrets = base64.b64encode(secrets).decode()
         encryption_key = f"{key_hash}:{encrypted_secrets}"
         return FileVault(
-            aes_options=seal_options,
             key_hash=key_hash,
             encryption_key=encryption_key,
             encrypted_file=encrypted_contents,
