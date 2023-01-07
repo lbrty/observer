a650d6757214b6f887dcbd2dd28113ac8d9996f8
Sultan Iman <sultan.imanhodjaev@gmail.com>
2023-01-07 13:09:48 +0100

    Refactor tests

--- tests/e2e_tests/test_pets.py
+++ tests/e2e_tests/test_pets.py
@@ -1,113 +1,38 @@
 import os.path
-import shutil
-from io import BytesIO
 
-from fastapi.encoders import jsonable_encoder
 from starlette import status
 
-from observer.common.types import PetStatus
-from observer.entities.permissions import NewPermission
-from observer.schemas.idp import NewIDPRequest
-from tests.helpers.crud import create_permission, create_pet, create_project
 
-
-async def test_delete_pets_deletes_all_related_documents(authorized_client, app_context, consultant_user, env_settings):
-    project = await create_project(app_context, "test project", "test description")
-    await create_permission(
-        app_context,
-        NewPermission(
-            can_create=True,
-            can_read=True,
-            can_update=True,
-            can_delete=True,
-            can_create_projects=True,
-            can_read_documents=True,
-            can_read_personal_info=True,
-            can_invite_members=True,
-            project_id=project.id,
-            user_id=consultant_user.id,
-        ),
-    )
-
-    payload = NewIDPRequest(
-        project_id=project.id,
-        email="Full_Name@examples.com",
-        full_name="Full Name",
-        phone_number="+11111111",
-        tags=["one", "two"],
-    )
-    resp = await authorized_client.post("/idp/people", json=jsonable_encoder(payload))
+async def test_delete_pets_deletes_all_related_documents(
+    authorized_client,
+    app_context,
+    consultant_user,
+    env_settings,
+    new_pet,
+    markdown_file,
+    fs_storage,
+):
+    files = {"file": ("readme.md", markdown_file, "text/markdown")}
+    resp = await authorized_client.post(f"/pets/{new_pet.id}/document", files=files)
     assert resp.status_code == status.HTTP_201_CREATED
 
-    owner_id = resp.json()["id"]
-    pet = await create_pet(
-        app_context,
-        "Jack",
-        PetStatus.needs_shelter,
-        "reg-id",
-        project.id,
-        owner_id,
-    )
-    # TODO: use tempdir
-    # TODO: test S3 storage
-    fp = BytesIO()
-    fp.write(b"BABA BLACK SHEEP")
-    fp.seek(0)
-    files = {"file": ("readme.md", fp, "text/markdown")}
-    resp = await authorized_client.post(f"/pets/{pet.id}/document", files=files)
-    assert resp.status_code == status.HTTP_201_CREATED
-
-    resp = await authorized_client.delete(f"/pets/{pet.id}")
+    resp = await authorized_client.delete(f"/pets/{new_pet.id}")
     assert resp.status_code == status.HTTP_204_NO_CONTENT
-    file_path = os.path.join(app_context.storage.root, env_settings.documents_path, str(pet.id))
+    file_path = os.path.join(app_context.storage.root, env_settings.documents_path, str(new_pet.id))
     assert os.path.exists(file_path) is False
 
 
-async def test_upload_document_for_pet_works(authorized_client, app_context, consultant_user, env_settings):
-    project = await create_project(app_context, "test project", "test description")
-    await create_permission(
-        app_context,
-        NewPermission(
-            can_create=True,
-            can_read=True,
-            can_update=True,
-            can_delete=True,
-            can_create_projects=True,
-            can_read_documents=True,
-            can_read_personal_info=True,
-            can_invite_members=True,
-            project_id=project.id,
-            user_id=consultant_user.id,
-        ),
-    )
-
-    payload = NewIDPRequest(
-        project_id=project.id,
-        email="Full_Name@examples.com",
-        full_name="Full Name",
-        phone_number="+11111111",
-        tags=["one", "two"],
-    )
-    resp = await authorized_client.post("/idp/people", json=jsonable_encoder(payload))
+async def test_upload_document_for_pet_works(
+    authorized_client,
+    app_context,
+    consultant_user,
+    new_pet,
+    markdown_file,
+    env_settings,
+    fs_storage,
+):
+    files = {"file": ("readme.md", markdown_file, "text/markdown")}
+    resp = await authorized_client.post(f"/pets/{new_pet.id}/document", files=files)
     assert resp.status_code == status.HTTP_201_CREATED
-
-    owner_id = resp.json()["id"]
-    pet = await create_pet(
-        app_context,
-        "Jack",
-        PetStatus.needs_shelter,
-        "reg-id",
-        project.id,
-        owner_id,
-    )
-    # TODO: use tempdir
-    # TODO: test S3 storage
-    fp = BytesIO()
-    fp.write(b"BABA BLACK SHEEP")
-    fp.seek(0)
-    files = {"file": ("readme.md", fp, "text/markdown")}
-    resp = await authorized_client.post(f"/pets/{pet.id}/document", files=files)
-    assert resp.status_code == status.HTTP_201_CREATED
-    file_path = os.path.join(app_context.storage.root, env_settings.documents_path, str(pet.id))
-    open(f"{file_path}/readme.md").read()
-    shutil.rmtree(app_context.storage.root)
+    file_path = os.path.join(app_context.storage.root, env_settings.documents_path, str(new_pet.id), "readme.md")
+    assert os.path.exists(file_path)
