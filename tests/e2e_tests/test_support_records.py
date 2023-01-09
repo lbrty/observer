964d4f3bf1194d4ba950e19896c0c4d5de2af4c2
Sultan Iman <sultan.imanhodjaev@gmail.com>
2023-01-09 13:57:11 +0100

    Prepare empty tests for support records

---
+++ tests/e2e_tests/test_support_records.py
@@ -0,0 +1,14 @@
+async def test_create_support_record_works():
+    ...
+
+
+async def test_get_support_record_works():
+    ...
+
+
+async def test_update_support_record_works():
+    ...
+
+
+async def test_delete_support_record_works():
+    ...
