3d4a374b31baaf99a8bae5b094be16fbcc696c1c
Sultan Iman <sultan.imanhodjaev@gmail.com>
2023-01-12 22:19:02 +0100

    Implement gmail mailer and asyncify sendgrid mailer

--- observer/services/mailer.py
+++ observer/services/mailer.py
@@ -1,8 +1,11 @@
+import asyncio
 import os
 from dataclasses import dataclass
+from email.message import EmailMessage as Message
 from typing import Protocol
 from urllib.error import HTTPError
 
+from aiosmtplib import send
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Content, From, Mail, To
 from structlog import get_logger
@@ -25,9 +28,21 @@ class IMailer(Protocol):
         raise NotImplementedError
 
 
-class Mailer(IMailer):
+class GmailMailer(IMailer):
+    port: int = 465
+    hostname: str = "smtp.gmail.com"
+
+    def __init__(self, username: str, password: str):
+        self.username = username
+        self.password = password
+
     async def send(self, message: EmailMessage):
-        pass
+        msg = Message()
+        msg["From"] = message.from_email
+        msg["To"] = message.to_email
+        msg["Subject"] = message.subject
+        msg.set_content(message.body)
+        await send(msg, hostname=self.hostname, port=self.port, use_tls=True)
 
 
 class SendgridMailer(IMailer):
@@ -38,14 +53,18 @@ class SendgridMailer(IMailer):
             raise InternalError(message="SENDGRID_API_KEY is not configured")
 
     async def send(self, message: EmailMessage):
-        try:
-            from_email = From(message.from_email)
-            to_email = To(message.to_email)
-            plain_text_content = Content("text/plain", message.body)
+        @asyncio.coroutine
+        def dispatch(msg: EmailMessage):
+            try:
+                from_email = From(msg.from_email)
+                to_email = To(msg.to_email)
+                plain_text_content = Content("text/plain", msg.body)
 
-            # instantiate `sendgrid.helpers.mail.Mail` objects
-            mail = Mail(from_email, to_email, plain_text_content)
-            self.client.send(mail)
-        except HTTPError as e:
-            logger.exception("Unable send Sendgrid email", response=e.read().decode())
-            raise InternalError(message="Unable send Sendgrid email")
+                # instantiate `sendgrid.helpers.mail.Mail` objects
+                mail = Mail(from_email, to_email, plain_text_content)
+                self.client.send(mail)
+            except HTTPError as e:
+                logger.exception("Unable send Sendgrid email", response=e.read().decode())
+                raise InternalError(message="Unable send Sendgrid email")
+
+        asyncio.run(dispatch(message))
