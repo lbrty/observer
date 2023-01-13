e47f81b135aae14a7efb7a981ebaa3e977f5f37e
Sultan Iman <sultan.imanhodjaev@gmail.com>
2023-01-13 22:19:10 +0100

    Implement family members service

---
+++ observer/services/family_members.py
@@ -0,0 +1,49 @@
+from typing import List, Protocol
+
+from observer.api.exceptions import NotFoundError
+from observer.common.types import Identifier
+from observer.entities.family_members import FamilyMember, NewFamilyMember
+from observer.repositories.family_members import IFamilyRepository
+from observer.schemas.family_members import NewFamilyMemberRequest
+
+
+class IFamilyService(Protocol):
+    repo: IFamilyRepository
+
+    async def add_member(self, new_member: NewFamilyMemberRequest) -> FamilyMember:
+        raise NotImplementedError
+
+    async def get_member(self, member_id: Identifier) -> FamilyMember:
+        raise NotImplementedError
+
+    async def get_by_person(self, idp_id: Identifier) -> List[FamilyMember]:
+        raise NotImplementedError
+
+    async def get_by_project(self, project_id: Identifier) -> List[FamilyMember]:
+        raise NotImplementedError
+
+    async def delete_member(self, member_id: Identifier) -> FamilyMember:
+        raise NotImplementedError
+
+
+class FamilyService(IFamilyService):
+    def __init__(self, repo: IFamilyRepository):
+        self.repo = repo
+
+    async def add_member(self, new_member: NewFamilyMemberRequest) -> FamilyMember:
+        return await self.repo.add_member(NewFamilyMember(**new_member.dict()))
+
+    async def get_member(self, member_id: Identifier) -> FamilyMember:
+        if member := await self.repo.get_member(member_id):
+            return member
+
+        raise NotFoundError(message="Family member not found")
+
+    async def get_by_person(self, idp_id: Identifier) -> List[FamilyMember]:
+        return await self.repo.get_by_person(idp_id)
+
+    async def get_by_project(self, project_id: Identifier) -> List[FamilyMember]:
+        return await self.repo.get_by_project(project_id)
+
+    async def delete_member(self, member_id: Identifier) -> FamilyMember:
+        return await self.repo.delete_member(member_id)
