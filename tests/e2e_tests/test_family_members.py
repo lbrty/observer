583e21376b2015383965fa3340fc14faf62d8be1
Sultan Iman <sultan.imanhodjaev@gmail.com>
2023-01-15 10:25:19 +0100

    Add family members test stubs

---
+++ tests/e2e_tests/test_family_members.py
@@ -0,0 +1,14 @@
+async def test_add_family_member_works():
+    pass
+
+
+async def test_get_family_members_works():
+    pass
+
+
+async def test_update_family_member_works():
+    pass
+
+
+async def test_delete_family_member_works():
+    pass
