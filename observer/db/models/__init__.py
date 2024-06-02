5ec06a58dc12935d5607e3ae4763f344641d4570
Sultan Iman <sultan.imanhodjaev@gmail.com>
2024-06-02 17:55:43 +0200

    Export available models

--- observer/db/models/__init__.py
+++ observer/db/models/__init__.py
@@ -1,4 +1,4 @@
-from .base import ModelBase
+from .base import ModelBase, TimestampedModel
 from .audit_logs import AuditLog
 from .auth import PasswordReset
 from .categories import Category
@@ -8,4 +8,25 @@ from .users import User
 from .documents import Document
 from .permissions import Permission
 from .pets import Pet
+from .people import People
+
 from .world import Country, State, Place
+
+
+__all__ = (
+    "ModelBase",
+    "TimestampedModel",
+    "AuditLog",
+    "PasswordReset",
+    "Category",
+    "Office",
+    "Project",
+    "User",
+    "Document",
+    "Permission",
+    "Pet",
+    "People",
+    "Country",
+    "State",
+    "Place",
+)
