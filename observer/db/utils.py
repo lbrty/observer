4cb5fa9258b57326f032a7e636e80935b6e19f0a
Sultan Iman <sultan.imanhodjaev@gmail.com>
2024-06-02 17:56:27 +0200

    Add database utils

--- observer/db/utils.py
+++ observer/db/utils.py
@@ -1,5 +1,14 @@
-from datetime import datetime, timezone
+from datetime import datetime, UTC
+from enum import Enum
+
+from observer.common.reflect.inspect import unwrap_enum
 
 
 def utcnow() -> datetime:
-    return datetime.now(tz=timezone.utc)
+    return datetime.now(tz=UTC)
+
+
+def choices_from_enum(enum: Enum) -> list[str]:
+    values = list(unwrap_enum(enum))
+    choices = [f"'{val}'" for val in values]
+    return choices
